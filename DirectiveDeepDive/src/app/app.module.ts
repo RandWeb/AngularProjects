b653fe71289a8b91976996cb28edae76d578b510
مهرداد تابش <mehrdadit12@gmail.com>
2023-05-05 04:03:57 -0700

    BasicHighlightDirective

--- DirectiveDeepDive/src/app/app.module.ts
+++ DirectiveDeepDive/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {BasicHighlightDirective} from "./basic-highlight/basic-highlight.directive";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule
+  ],
+  exports:[
+    BasicHighlightDirective
   ],
   providers: [],
   bootstrap: [AppComponent]
