f7f3c89b3c4c4c5a8c52ec1e62c861bf6c445bd0
مهرداد تابش <mehrdadit12@gmail.com>
2023-05-05 06:31:19 -0700

    ngSwitch

--- DirectiveDeepDive/src/app/app.component.ts
+++ DirectiveDeepDive/src/app/app.component.ts
@@ -11,4 +11,5 @@ export class AppComponent {
   numbers: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   oddNumbers: Array<number> = [1, 3, 5, 7, 9];
   evenNumbers: Array<number> = [2, 4, 6, 8, 10];
+  value:number = 14;
 }
