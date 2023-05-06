6c6ae6c9047019fffd206d17edbbcd58cc73fe08
مهرداد تابش <mehrdadit12@gmail.com>
2023-05-06 02:56:06 -0700

    send and get query param with route

--- ChangeingPage/src/app/servers/server/server.component.ts
+++ ChangeingPage/src/app/servers/server/server.component.ts
@@ -10,11 +10,14 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server!: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService) { }
 
   ngOnInit() {
     // @ts-ignore
     this.server = this.serversService.getServer(1);
   }
+
+
 
 }
